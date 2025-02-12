5a4e19b3476345e75e64637e720434ad1305220b
berFiras <firasberrajah94@gmail.com>
2025-02-12 06:06:34 +0100

    Update: Pokemon model

--- src/models/front/Pokemon.model.ts
+++ src/models/front/Pokemon.model.ts
@@ -4,22 +4,24 @@ class Pokemon {
   id: number;
   name: string;
   image: string;
-  hp: number;
-  attack: number;
-  defense: number;
-  speed: number;
+  stats: { name: string; value: number }[];
   types: string[];
 
   constructor(ref: PokemonDTO) {
     this.id = ref.id;
     this.name = ref.name;
     this.image = ref.sprites?.[0].sprite?.back_default || "";
-    this.hp = ref.stats?.[0]?.base_stat || 0;
-    this.attack = ref.stats?.[1]?.base_stat || 0;
-    this.defense = ref.stats?.[2]?.base_stat || 0;
-    this.speed = ref.stats?.[5]?.base_stat || 0;
+    this.stats = defineStats(ref) ?? [];
     this.types = ref.types?.map((t) => t.type.name) || [];
   }
+}
+
+function defineStats(ref: PokemonDTO) {
+  if (!ref?.stats) return;
+  return ref?.stats?.map((elm) => ({
+    name: elm.stat_name.name,
+    value: elm.base_stat,
+  }));
 }
 
 export default Pokemon;
