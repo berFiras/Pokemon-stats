44b6449d129c0ab76f006c545445c8926dcdbf36
berFiras <firasberrajah94@gmail.com>
2025-02-12 07:08:31 +0100

    Add: fetch pokemon list

--- src/components/molecules/pokemon-details/PokemonDetails.component.tsx
+++ src/components/molecules/pokemon-details/PokemonDetails.component.tsx
@@ -7,26 +7,29 @@ const PokemonDetails = (props: PokemonDetailsProps) => {
   const { pokemon } = props;
 
   return (
-    <div className="p-6 border rounded-xl shadow-lg bg-white dark:bg-gray-800 hover:shadow-xl transition-shadow duration-300 w-80">
+    <div className="p-2 border rounded-xl shadow-lg bg-white dark:bg-gray-800 hover:shadow-xl transition-shadow duration-300 w-80">
       <div className="flex justify-center">
         <Image
           src={pokemon.image}
           alt={pokemon.name ?? "name"}
-          className="w-48 h-48 object-contain mb-4"
+          className="w-36 h-36 object-contain"
         />
       </div>
 
       <h2 className="text-2xl font-bold text-center capitalize text-gray-800 dark:text-white mb-4">
-        {pokemon.name}
+        {`#${pokemon.id} ${pokemon.name}`}
       </h2>
 
-      <div className="space-y-2">
+      <div className="grid grid-cols-3 gap-2">
         {pokemon.stats?.map((stat) => (
-          <div key={stat.name} className="flex justify-between">
-            <Text className="text-sm font-medium text-gray-600 dark:text-gray-300 capitalize">
-              {stat.name}:
+          <div
+            key={stat.name}
+            className="flex flex-col items-center bg-gray-200 dark:bg-gray-700 p-2 rounded-lg"
+          >
+            <Text className="text-xs font-medium text-gray-600 dark:text-gray-300 capitalize">
+              {stat.name}
             </Text>
-            <Text className="text-sm font-semibold text-gray-800 dark:text-white">
+            <Text className="text-sm font-bold text-gray-800 dark:text-white">
               {stat.value}
             </Text>
           </div>
